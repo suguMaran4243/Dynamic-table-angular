30ecbf19d0cc159d8df948a3ed66145d45da78c6
suguMaranram <sugumaran.ram@aspiresys.com>
2024-02-19 21:10:58 +0530

    tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { headerNamings,studentData,data} from './table-data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table';
+  
+  headings=headerNamings;
+  data:data[]=studentData;
 }
---
+++ src/app/table-data.ts
@@ -0,0 +1,16 @@
+
+   export const  headerNamings=[
+    
+          { key:'id',label:'Student ID'}  ,
+          { key:'name',label:'Student Name'},
+           {key:'age',label:'Student age'}
+           
+    ]
+  export type data={id:number;name:string;age:number}
+   export const  studentData:data[]=[
+        {id:101,name:'Sugumaran',age:22},
+        {id:102,name:'Thirumurugan',age:18},
+        {id:103,name:'Sathurganth',age:22},
+    ]
+
+
